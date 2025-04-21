a0f4fcf61f03a6a7a927850af930125be6b15eb2
JetDev22 <thomas.roelig@gmail.com>
2025-04-21 20:48:01 +0200

    finished converter

---
+++ src/test_converter.py
@@ -0,0 +1,16 @@
+import unittest
+
+from converter import *
+from textnode import TextNode, TextType 
+from htmlnode import HTMLNode 
+
+
+class Converter(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+if __name__ == "__main__":
+    unittest.main()
